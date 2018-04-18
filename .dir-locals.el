47bc60234a819951ce6b56836f45e27027647b10
Emil Segerbäck <emilsegerback@gmail.com>
2018-04-18 16:22:23 +0200

    Add .dir-locals with flycheck settings

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-gcc-include-path .
+                               ("/home/em/Programmering/skola/tnm079-student-master")))
+ (c-mode
+  (mode . c++)))
+
