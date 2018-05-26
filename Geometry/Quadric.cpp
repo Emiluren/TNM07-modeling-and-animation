e1015f4804f174e5d92efe53d3f028eef8022547
Emil Segerbäck <emilsegerback@gmail.com>
2018-05-26 15:55:24 +0200

    Lab 4: Implement the quadric surface

--- Geometry/Quadric.cpp
+++ Geometry/Quadric.cpp
@@ -20,11 +20,26 @@ Quadric::~Quadric() {}
  * of the world-coordinates by mWorld2Obj, or transformation of the quadric
  * coefficient matrix by GetTransform() ONCE (see Section 2.2 in lab text).
  */
-float Quadric::GetValue(float x, float y, float z) const { return 0; }
+float Quadric::GetValue(float x, float y, float z) const {
+  TransformW2O(x, y, z);
+  Vector4<float> p = Vector4<float>(x, y, z, 1);
+  float res = (mQuadric * p) * p;
+  return res;
+}
 
 /*!
  * Use the quadric matrix to evaluate the gradient.
  */
 Vector3<float> Quadric::GetGradient(float x, float y, float z) const {
-  return Vector3<float>(0, 0, 0);
+  TransformW2O(x, y, z);
+  Vector4<float> p = Vector4<float>(x, y, z, 1);
+
+  Vector3<float> res(0, 0, 0);
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 4; col++) {
+      res[row] += mQuadric(row, col) * p[row];
+    }
+  }
+
+  return res * 2;
 }
